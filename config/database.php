dbb4a2555366e7516a5a400a930fbea61665ad91
guilherme-ubu <guilhermepereiradv@gmail.com>
2023-03-06 09:28:06 -0300

    update composer and npm dependencies and docker compose files

--- config/database.php
+++ config/database.php
@@ -46,9 +46,9 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
